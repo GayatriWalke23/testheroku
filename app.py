8bcb8f5d7bd10dafc77bd1d1b8a0efeae324d6ef
GayatriWalke23 <gayatri.walke@cumminscollege.in>
2022-04-30 13:46:20 +0530

    make it better3

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+print(app)
+
+@app.route('/',methods=['POST', 'GET'])
+def index():
+    if request.method == "POST":
+        #<insert python script here>
+        print("hi")
+    #exec(open('main.py').read())
+    return render_template('index.html')
+
+@app.route('/foo', methods=['GET', 'POST'])
+def foo():
+    #print("hi")
+    #exec(open('main.py').read())
+    '''execute whatever code you want when the button gets clicked here'''
+    return render_template('index.html')
+if __name__ == "__main__":
+    #exec(open('main.py').read())
+    app.run()
